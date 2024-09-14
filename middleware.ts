daa0ee04a1969cd86eafd32810c3e3f6d9c42e00
khrisnaa <igdkhrisnaaa@gmail.com>
2024-09-14 12:41:04 +0800

    fix: update protexted routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,33 @@
-export { auth as middleware } from '@/auth';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { auth as middleware } from '@/auth';
+import { NextResponse } from 'next/server';
+
+const protectedRoutes = [
+  '/trips',
+  '/reservations',
+  '/properties',
+  '/favorites',
+];
+
+export default middleware((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isProtectedRoutes = protectedRoutes.includes(nextUrl.pathname);
+
+  if (isProtectedRoutes) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL('/', nextUrl));
+    }
+  }
+});
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/trips',
+    '/reservations',
+    '/properties',
+    '/favorites',
+  ],
 };
